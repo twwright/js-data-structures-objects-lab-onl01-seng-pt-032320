7b99633b56a2b2ab2e91c2f8cc1c21b2125e8143
Thomas Wright <twwright@gmail.com>
2020-09-29 17:41:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const driver = {};
 function updateDriverWithKeyAndValue(driverObj, key, value) {
   
 }
+
+
+// function nondestructivelyUpdateObject(obj, key, value) {
+//   return Object.assign({}, obj, { [key]: value });
+// }
