f83610d3e8f5f6e9c106abd4acdfdfc7dec0c342
Thomas Wright <twwright@gmail.com>
2020-09-29 17:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driverObj, key, value) {
-  
+  return Object.assign({}, driverObj, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driverObj, key, value) {
+  driverObj[key] = value;
+}
+
+function deleteFromDriverByKey(driverObj, key) {
+  let newDriver = Object.assign({}, driverObj);
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driverObj, key) {
+  delete driverObj[key];
 }
 
 
 // function nondestructivelyUpdateObject(obj, key, value) {
 //   return Object.assign({}, obj, { [key]: value });
 // }
-
 
 
 // function destructivelyUpdateObject (obj, key, value) {
